ca93a1c354c9a90f3b4452586d28dd082cc41280
guoziyang <gzygzy991030@yahoo.com>
2021-05-26 20:55:26 +0800

    add new sample code

---
+++ parse/parse.h
@@ -0,0 +1,8 @@
+//
+// Created by GuoZiYang on 2021/5/26.
+//
+
+#ifndef POSFOL_PARSE_H
+#define POSFOL_PARSE_H
+
+#endif //POSFOL_PARSE_H
---
+++ parse/token.h
@@ -0,0 +1,8 @@
+//
+// Created by GuoZiYang on 2021/5/26.
+//
+
+#ifndef POSFOL_TOKEN_H
+#define POSFOL_TOKEN_H
+
+#endif //POSFOL_TOKEN_H
