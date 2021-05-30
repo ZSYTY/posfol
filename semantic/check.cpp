7ba6f6c0f4737299233a026ca377e24b369e5ab1
gehao <Sakura.gehao@gmail.com>
2021-05-30 21:13:56 +0800

    add semantic check traverse template

---
+++ semantic/check.cpp
@@ -0,0 +1,85 @@
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "node.h"
+#include "sem_util.h"
+#include "symbol.h"
+#include "type.h"
+
+extern Block* programBlock;
+
+SymbolTable symbolTable;
+
+/**
+ * @author gehao 
+ */
+bool checkProgram(Block* programBlock) {
+    return check(programBlock);
+}
+
+/**
+ * @author gehao
+ * 
+ * 每进入一层block，就创建一个symbolTable的map压入栈中，表示新的一级变量作用域
+ */
+bool check(Block* root) {
+    symbolTable.push();
+    std::vector<Statement*>* stmts = root->getStatementList();
+
+    for (auto it = stmts->begin(); it != stmts->end(); it++) {
+        Statement* stmt = *it;
+        if (instanceof <Declaration>(stmt)) {
+            if (instanceof <VariableDeclaration>(stmt)) {
+                ;
+            } else if (instanceof <FunctionDeclaration>(stmt)) {
+                ;
+            } else if (instanceof <ClassDeclaration>(stmt)) {
+                ;
+            } else {
+                return false;
+            }
+        } else if (instanceof <Expression>(stmt)) {
+            if (instanceof <Identifier>(stmt)) {
+                ;
+            } else if (instanceof <FuncCallExpression>(stmt)) {
+                ;
+            } else if (instanceof <ArithmeticExpression>(stmt)) {
+                if (instanceof <BinaryOperator>(stmt)) {
+                    ;
+                } else {
+                    return false;
+                }
+            } else if (instanceof <AssignExpression>(stmt)) {
+                if (instanceof <VariableAssign>(stmt)) {
+                    ;
+                } else if (instanceof <ArrayAssign>(stmt)) {
+                    ;
+                } else if (instanceof <ClassAssign>(stmt)) {
+                    ;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } else if (instanceof <LogicStatement>(stmt)) {
+            if (instanceof <IfStatement>(stmt)) {
+                ;
+            } else if (instanceof <ForStatement>(stmt)) {
+                ;
+            } else if (instanceof <WhileStatement>(stmt)) {
+                ;
+            } else {
+                return false;
+            }
+        } else if (instanceof <Block>(stmt)) {
+            // 进入新的block，递归检查
+            return check(dynamic_cast<Block*>(stmt));
+        } else {
+            return false;
+        }
+    }
+
+    symbolTable.pop();
+}
---
+++ semantic/check.h
@@ -0,0 +1,9 @@
+#ifndef __CHECK_H__
+#define __CHECK_H__
+
+#include "node.h"
+#include "type.h"
+
+bool checkProgram(Block* programBlock);
+bool check(Block* root);
+#endif
