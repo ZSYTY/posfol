d5373c37e5e1e337a47c0fe81b1dafae4f4e99cf
gehao <Sakura.gehao@gmail.com>
2021-06-04 20:21:10 +0800

    update symbol table

--- semantic/symbol.cpp
+++ semantic/symbol.cpp
@@ -1,5 +1,7 @@
 #include "symbol.h"
 
+#include "sem_util.h"
+
 Declaration* SymbolTable::findSymbol(std::string name) {
     for (auto it = symbolTable.rbegin(); it != symbolTable.rend(); it++) {
         if (it->count(name) != 0) {
@@ -9,15 +11,47 @@ Declaration* SymbolTable::findSymbol(std::string name) {
     return nullptr;
 }
 
-void SymbolTable::push() {
+Type SymbolTable::getSymbolType(std::string name) {
+    Declaration* decl = this->findSymbol(name);
+    if (decl == nullptr) {
+        return ERROR;
+    } else {
+        if (instanceof <VariableDeclaration>(decl)) {
+            VariableDeclaration* var = dynamic_cast<VariableDeclaration*>(decl);
+            return var->getVarType()->getType();
+        } else if (instanceof <FunctionDeclaration>(decl)) {
+            return FUNCTION;
+        } else if (instanceof <ClassDeclaration>(decl)) {
+            return CLASS;
+        } else if (instanceof <InterfaceDeclaration>(decl)) {
+            return INTERFACE;
+        } else {
+            return ERROR;
+        }
+    }
+}
+
+bool SymbolTable::isDeclared(std::string name) {
+    return this->top().count(name) != 0;
+}
+
+void SymbolTable::pushAR() {
     std::unordered_map<std::string, Declaration*> curMap;
     symbolTable.push_back(curMap);
 }
 
-void SymbolTable::pop() {
+void SymbolTable::popAR() {
     symbolTable.pop_back();
 }
 
 std::unordered_map<std::string, Declaration*> SymbolTable::top() {
     return symbolTable.back();
+}
+
+void SymbolTable::push(std::string name, Declaration* decl) {
+    this->top()[name] = decl;
+}
+
+void SymbolTable::pop(std::string name) {
+    this->top().erase(name);
 }
--- semantic/symbol.h
+++ semantic/symbol.h
@@ -25,14 +25,23 @@ class SymbolTable {
    public:
     SymbolTable() {}
     ~SymbolTable() {}
-
+    // 寻找该符号是否存在
     Declaration* findSymbol(std::string name);
-
-    void push();
-
-    void pop();
-
+    // 如果是变量则返回具体类型，如果是函数、类、接口，则返回对应抽象类型
+    Type getSymbolType(std::string name);
+    // 判断在当前作用域内是否已经声明，注：在外层作用域声明的话，本层作用域会用新的symbol引用覆盖
+    bool isDeclared(std::string name);
+    // 往栈里新push一个AR，表示进入一个新的作用域(Block)
+    void pushAR();
+    // 从栈顶pop一个AR，表示跳出该作用域(Block)
+    void popAR();
+    // 获取栈顶的AR，即当前作用域(Block)
     std::unordered_map<std::string, Declaration*> top();
+
+    // push新的Declaration
+    void push(std::string name, Declaration* decl);
+    // pop旧的Declaration
+    void pop(std::string name);
 };
 
 #endif
