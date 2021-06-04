19217213210a632600d942adaac4d46b89c7d1b8
gehao <Sakura.gehao@gmail.com>
2021-06-04 20:55:45 +0800

    solve the template compile error

--- semantic/sem_util.h
+++ semantic/sem_util.h
@@ -1,7 +1,15 @@
 #ifndef __SEMUTIL_H__
 #define __SEMUTIL_H__
 
+/**
+ * @author gehao
+ * 
+ * instanceof<Parent>(p)：判断p指向的对象是否为Parent的子类(含Parent) 
+ * template的实现也要放在.h文件，否则make会报错
+ */
 template <typename Base, typename T>
-inline bool instanceof (T * p);
+inline bool instanceof (T * p) {
+    return dynamic_cast<Base*>(p);
+}
 
 #endif
